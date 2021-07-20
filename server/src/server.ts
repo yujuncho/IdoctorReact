6514fe04e7843681c6845eadc8247b100e36f92d
Yujun <yujuncho7@gmail.com>
2021-07-20 15:32:52 -0700

    Updated server to use TypeScript

---
+++ server/src/server.ts
@@ -0,0 +1,12 @@
+import express from "express";
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "../../client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
